042ba638a1857e82a996ce0ec924224cc1a1008b
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-23 10:52:14 +0800

    docs: add symbol docs

--- index.d.ts
+++ index.d.ts
@@ -14,9 +14,15 @@ type Next = (
 
 type Middleware = (next: Next) => Promise<void>;
 
+/** This function allows you to add a command */
 export function add(callback: Callback, options: Options): number;
+/** This function allows you to initialize your bot setup */
 export function init(option?: {}): void;
+/** This function returns the list of comands currently registered */
 export function list(): Options[];
+/** This function allows you to register a new middleware */
 export function addMiddleware(middleware: Middleware): number;
+/** This function allows you to register a new event middleware */
 export function addEventMiddleware(middleware: Middleware): number;
+/** This function allows you to add a listener to an event */
 export function on(event: string, listener: Listener, args?: []): void;
